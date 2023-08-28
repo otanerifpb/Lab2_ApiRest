cb6930f7a2ff7c23f13147e9d04a58c6f59f9352
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-08-28 01:29:13 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifpb/progdist/lab2_apirest/HelloSpringController.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.progdist.lab2_apirest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloSpringController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
+}
---
+++ src/test/java/br/edu/ifpb/progdist/lab2_apirest/Lab2ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.progdist.lab2_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
